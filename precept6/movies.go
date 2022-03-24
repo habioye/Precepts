8f77308a5ea26e956bf4e00dc129f39e4b4ed456
habioye <78454514+habioye@users.noreply.github.com>
2022-03-24 15:44:29 -0400

    Update movies.go

--- precept6/movies.go
+++ precept6/movies.go
@@ -3,8 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func listMoviesGenresByYear(db *sql.DB, year int) {
